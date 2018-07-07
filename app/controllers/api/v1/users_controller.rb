53b48b9846bc110b8dbde6836ae4526e610b2206
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-07 12:05:51 -0400

    builds out full CRUD actions for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,31 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def create
+    user = User.new(user_params)
+    user.save
   end
 
   def show
+    
   end
 
   def update
+    
   end
 
   def destroy
+    user.destroy
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
+  end
+
+  def set_user
+    user = User.find(params[:id])
+  end
+  
 end
