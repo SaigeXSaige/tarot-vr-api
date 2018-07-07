6fcb9251fa20d50e7496fa5f3cbc91e116f83859
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-07 12:29:02 -0400

    validates email and username uniqueness, password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-
+    
     has_many :readings
+    validates :username, uniqueness: true
+    validates :password, length: {minimum:8, maximum: 18}
 end
