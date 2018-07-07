0324f717224da9d3d8aca650e52b3b3f61a784e2
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-07 11:14:05 -0400

    provides code to populate database with cards by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# POPULATES DATABASE WITH DEFAULT CARDS
+# url = 'https://tarot.howlcode.com/'
+# response = RestClient.get(url)
+# tarot_data = JSON.parse(response)
+
+# def convert_hash(obj)
+#     Hash[name: obj["name"], meaning: obj["short_meaning"], image_url: obj["face_image_url"], reverse: obj["reverse"], up: obj["up"], full_meaning: obj["full_meaning"]]
+# end
+
+# tarot_data.each { |card| Card.create(convert_hash(card))}
