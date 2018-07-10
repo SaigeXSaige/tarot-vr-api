cb7a659338e17634b904afbe6b672f0be9461d58
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-10 17:09:12 -0400

    leaves comment for error catching reminder

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,7 @@ class Api::V1::UsersController < ApplicationController
   def update
     if @user.update(user_params)
       render json: @user
+      # else: catch and show error
     end
   end
 
@@ -23,7 +24,7 @@ class Api::V1::UsersController < ApplicationController
   private 
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
+    params.permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
   end
 
   def set_user
