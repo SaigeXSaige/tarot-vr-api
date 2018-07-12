73d27212aa4758e235400aecc02c8a3b8f6f3074
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-11 22:07:36 -0400

    uses jwt token instead of id

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,26 +1,11 @@
-# class Api::V1::AuthController 
-
-#     def create
-#         user = User.find_by(username: params[:username])
-
-#         if user && user.authenticat(params[:password])
-#             render json: {username: user.username, id: user.id}, status: 200
-#         else
-#             render json: {error: 'Invalid Password or Username'}, status: 401
-#         end
-        
-#     end
-    
-
-# end
-
 class Api::V1::AuthController < ApplicationController
 
     def create
       user = User.find_by(username: params[:username])
       
       if user && user.authenticate(params[:password])
-        render json: {username: user.username, id: user.id}, status: 200
+        token = encoded_token(user)
+        render json: {username: user.username, id: user.id, jwt: token}, status: 200
       else
         render json: {error: 'Username or Password Invalid'}, status: 401
       end
