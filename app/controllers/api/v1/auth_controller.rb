5927001739ecdd0874fc95837e15d42eb216d3da
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-12 13:26:00 -0400

    auth now sends more user information to frontend

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthController < ApplicationController
       
       if user && user.authenticate(params[:password])
         token = encoded_token(user)
-        render json: {username: user.username, id: user.id, jwt: token}, status: 200
+        render json: {username: user.username, id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email, readings: user.readings, jwt: token}, status: 200
       else
         render json: {error: 'Username or Password Invalid'}, status: 401
       end
@@ -13,7 +13,7 @@ class Api::V1::AuthController < ApplicationController
   
     def show
       if logged_in
-        render json: {username: current_user.username, id: current_user.id}, status: 200
+        render json: {username: current_user.username, id: current_user.id, first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email, readings: current_user.readings }, status: 200
       else
         render json: {error: 'Token Invalid'}, status: 401
       end
