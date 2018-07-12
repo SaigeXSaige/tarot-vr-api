dbc2ea067b994d87b6602e742506a2980906d29d
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-11 22:04:42 -0400

    uncomments seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 # POPULATES DATABASE WITH DEFAULT CARDS
-# url = 'https://tarot.howlcode.com/'
-# response = RestClient.get(url)
-# tarot_data = JSON.parse(response)
+url = 'https://tarot.howlcode.com/'
+response = RestClient.get(url)
+tarot_data = JSON.parse(response)
 
-# def convert_hash(obj)
-#     Hash[name: obj["name"], meaning: obj["short_meaning"], image_url: obj["face_image_url"], reverse: obj["reverse"], up: obj["up"], full_meaning: obj["full_meaning"]]
-# end
+def convert_hash(obj)
+    Hash[name: obj["name"], meaning: obj["short_meaning"], image_url: obj["face_image_url"], reverse: obj["reverse"], up: obj["up"], full_meaning: obj["full_meaning"]]
+end
 
-# tarot_data.each { |card| Card.create(convert_hash(card))}
+tarot_data.each { |card| Card.create(convert_hash(card))}
